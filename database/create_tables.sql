53f2bd3de78ed61655f534e98e7d8f71109f704b
Josselin Muller <josselin.muller@groupm.com>
2017-04-23 00:46:14 -0400

    Updating database script with tags

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,81 +1,100 @@
 USE [HermesMaster]
 GO
-/****** Object:  Table [dbo].[application]    Script Date: 23/04/2017 00:00:54 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[application](
-	[application_id] [int] IDENTITY(1,1) NOT NULL,
-	[application_name] [nvarchar](128) NOT NULL,
- CONSTRAINT [PK_dbo.application] PRIMARY KEY CLUSTERED 
-(
-	[application_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
 
-GO
-/****** Object:  Table [dbo].[channel]    Script Date: 23/04/2017 00:00:54 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[channel](
-	[channel_id] [int] IDENTITY(1,1) NOT NULL,
-	[channel_name] [nvarchar](128) NOT NULL,
-	[application_id] [int] NOT NULL,
- CONSTRAINT [PK_dbo.channel] PRIMARY KEY CLUSTERED 
-(
-	[channel_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
 
-GO
-/****** Object:  Table [dbo].[notification]    Script Date: 23/04/2017 00:00:54 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[notification](
-	[notification_id] [bigint] IDENTITY(1,1) NOT NULL,
-	[notification_time] [datetime] NOT NULL,
-	[notification_message] [nvarchar](256) NOT NULL,
-	[channel_id] [int] NOT NULL,
- CONSTRAINT [PK_dbo.notification] PRIMARY KEY CLUSTERED 
-(
-	[notification_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+CREATE TABLE [application]
+( 
+	[application_id]     int  NOT NULL  IDENTITY ( 1,1 ) ,
+	[application_name]   nvarchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL 
+)
+go
 
-GO
-/****** Object:  Table [dbo].[notification_client_receipt]    Script Date: 23/04/2017 00:00:54 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[notification_client_receipt](
-	[notification_id] [bigint] NOT NULL,
-	[client_id] [uniqueidentifier] NOT NULL,
- CONSTRAINT [PK_dbo.notification_client_receipt] PRIMARY KEY CLUSTERED 
-(
-	[notification_id] ASC,
-	[client_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+ALTER TABLE [application]
+	ADD CONSTRAINT [PK_application] PRIMARY KEY  CLUSTERED ([application_id] ASC)
+go
 
-GO
-ALTER TABLE [dbo].[channel]  WITH CHECK ADD  CONSTRAINT [FK_dbo.channel_dbo.application_application_id] FOREIGN KEY([application_id])
-REFERENCES [dbo].[application] ([application_id])
-GO
-ALTER TABLE [dbo].[channel] CHECK CONSTRAINT [FK_dbo.channel_dbo.application_application_id]
-GO
-ALTER TABLE [dbo].[notification]  WITH CHECK ADD  CONSTRAINT [FK_dbo.notification_dbo.channel_channel_id] FOREIGN KEY([channel_id])
-REFERENCES [dbo].[channel] ([channel_id])
-GO
-ALTER TABLE [dbo].[notification] CHECK CONSTRAINT [FK_dbo.notification_dbo.channel_channel_id]
-GO
-ALTER TABLE [dbo].[notification_client_receipt]  WITH CHECK ADD  CONSTRAINT [FK_dbo.notification_client_receipt_dbo.notification_notification_id] FOREIGN KEY([notification_id])
-REFERENCES [dbo].[notification] ([notification_id])
-GO
-ALTER TABLE [dbo].[notification_client_receipt] CHECK CONSTRAINT [FK_dbo.notification_client_receipt_dbo.notification_notification_id]
-GO
+CREATE TABLE [channel]
+( 
+	[channel_id]         bigint  NOT NULL  IDENTITY ( 1,1 ) ,
+	[channel_name]       nvarchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL ,
+	[application_id]     int  NOT NULL 
+)
+go
+
+ALTER TABLE [channel]
+	ADD CONSTRAINT [PK_channel] PRIMARY KEY  CLUSTERED ([channel_id] ASC)
+go
+
+CREATE TABLE [notification]
+( 
+	[notification_id]    bigint  NOT NULL  IDENTITY ( 1,1 ) ,
+	[notification_code]  varchar(128)  NOT NULL ,
+	[notification_time]  datetime  NOT NULL ,
+	[notification_message] nvarchar(256) COLLATE SQL_Latin1_General_CP1_CI_AS  NOT NULL ,
+	[channel_id]         bigint  NOT NULL 
+)
+go
+
+ALTER TABLE [notification]
+	ADD CONSTRAINT [PK_notification] PRIMARY KEY  CLUSTERED ([notification_id] ASC)
+go
+
+CREATE TABLE [notification_client_receipt]
+( 
+	[notification_id]    bigint  NOT NULL ,
+	[client_id]          uniqueidentifier  NOT NULL 
+)
+go
+
+ALTER TABLE [notification_client_receipt]
+	ADD CONSTRAINT [PK_notification_client_receipt] PRIMARY KEY  CLUSTERED ([notification_id] ASC,[client_id] ASC)
+go
+
+CREATE TABLE [notification_tag]
+( 
+	[notification_tag_id] bigint  NOT NULL  IDENTITY ,
+	[notification_id]    bigint  NOT NULL ,
+	[notification_tag_key] varchar(128)  NOT NULL ,
+	[notification_tag_value] varchar(128)  NOT NULL 
+)
+go
+
+ALTER TABLE [notification_tag]
+	ADD CONSTRAINT [PK_notification_tag] PRIMARY KEY  CLUSTERED ([notification_tag_id] ASC)
+go
+
+ALTER TABLE [notification_tag]
+	ADD CONSTRAINT [UX_notification_tag] UNIQUE ([notification_id]  ASC,[notification_tag_key]  ASC)
+go
+
+
+ALTER TABLE [channel] WITH CHECK 
+	ADD CONSTRAINT [FK_channel__application] FOREIGN KEY ([application_id]) REFERENCES [application]([application_id])
+go
+
+ALTER TABLE [channel]
+	  WITH CHECK CHECK CONSTRAINT [FK_channel__application]
+go
+
+
+ALTER TABLE [notification] WITH CHECK 
+	ADD CONSTRAINT [FK_notification__channel] FOREIGN KEY ([channel_id]) REFERENCES [channel]([channel_id])
+go
+
+ALTER TABLE [notification]
+	  WITH CHECK CHECK CONSTRAINT [FK_notification__channel]
+go
+
+
+ALTER TABLE [notification_client_receipt] WITH CHECK 
+	ADD CONSTRAINT [FK_notification_client_receipt__notification] FOREIGN KEY ([notification_id]) REFERENCES [notification]([notification_id])
+go
+
+ALTER TABLE [notification_client_receipt]
+	  WITH CHECK CHECK CONSTRAINT [FK_notification_client_receipt__notification]
+go
+
+
+ALTER TABLE [notification_tag]
+	ADD CONSTRAINT [FK_notification_key__notification] FOREIGN KEY ([notification_id]) REFERENCES [notification]([notification_id])
+go
